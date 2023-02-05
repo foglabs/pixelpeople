6edf4a9aea5931a9baed7ff340dd5fb524b74f94
foglabs <foglaboratories@gmail.com>
2023-02-05 01:36:54 -0500

    'rearrange'

--- src/classes/SimpleControl.js
+++ src/classes/SimpleControl.js
@@ -5,7 +5,6 @@ function SimpleControl(props){
       <button onTouchStart={ () => props.startHoldDown(props.changeFunction, props.fieldName, props.increment, props.round)  } onMouseDown={ () => props.startHoldDown(props.changeFunction, props.fieldName, props.increment, props.round) } onClick={ () => props.changeFunction(props.value+props.increment) } >▲</button>
       <button onTouchStart={ () => props.startHoldDown(props.changeFunction, props.fieldName, -props.increment, props.round) } onMouseDown={ () => props.startHoldDown(props.changeFunction, props.fieldName, -props.increment, props.round) } onClick={ () => props.changeFunction(props.value-props.increment) } >▼</button>
       <div>{ props.label }</div>
-      <div>{ (Math.round(props.value * 100) / 100).toFixed(2) }</div>
     </div>
 
   )
@@ -14,7 +13,7 @@ function SimpleControl(props){
 export default SimpleControl
 
 
-
+// <div>{ (Math.round(props.value * 100) / 100).toFixed(2) }</div>
 
 // onTouchEnd={ props.stopHoldDown } onMouseUp={ props.stopHoldDown }
 
